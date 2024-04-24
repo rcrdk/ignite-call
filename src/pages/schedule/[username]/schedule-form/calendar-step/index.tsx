c3ace41e2f9cf5b79f9b14f5b4ab10b852c070db
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2024-04-24 16:31:22 -0300

    Progress on scheduling

---
+++ src/pages/schedule/[username]/schedule-form/calendar-step/index.tsx
@@ -0,0 +1,51 @@
+import dayjs from 'dayjs'
+import { useState } from 'react'
+
+import { Calendar } from '@/components/calendar'
+
+import {
+	Container,
+	TimePicker,
+	TimePickerHeader,
+	TimePickerItem,
+	TimePickerList,
+} from './styles'
+
+export function CalendarStep() {
+	const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+
+	const hasSelectedDate = !!selectedDate
+
+	const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+	const stringDate = selectedDate
+		? dayjs(selectedDate).format('DD[ de ]MMMM')
+		: null
+
+	return (
+		<Container isTimePickerOpen={hasSelectedDate}>
+			<Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
+
+			{hasSelectedDate && (
+				<TimePicker>
+					<TimePickerHeader>
+						{weekDay} <span>{stringDate}</span>
+					</TimePickerHeader>
+
+					<TimePickerList>
+						<TimePickerItem>08:00h</TimePickerItem>
+						<TimePickerItem>09:00h</TimePickerItem>
+						<TimePickerItem>10:00h</TimePickerItem>
+						<TimePickerItem>11:00h</TimePickerItem>
+						<TimePickerItem>12:00h</TimePickerItem>
+						<TimePickerItem>13:00h</TimePickerItem>
+						<TimePickerItem>14:00h</TimePickerItem>
+						<TimePickerItem>15:00h</TimePickerItem>
+						<TimePickerItem>16:00h</TimePickerItem>
+						<TimePickerItem>17:00h</TimePickerItem>
+						<TimePickerItem>18:00h</TimePickerItem>
+					</TimePickerList>
+				</TimePicker>
+			)}
+		</Container>
+	)
+}
