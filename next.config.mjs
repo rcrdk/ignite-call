f4e5f641b0d0d6ddacbf3c8a5b6da79b7fe567b5
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2024-04-29 15:19:04 -0300

    Add modularizImports to fix icons error on propduction

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+	modularizeImports: {
+		'@phosphor-icons/react': {
+			transform: '@phosphor-icons/react/dist/ssr{{member}}',
+		},
+	},
 }
 
 export default nextConfig
